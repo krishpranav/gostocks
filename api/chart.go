3cb74dd31734bd5cd8aeb4e0cf284d48c9a8552d
krishpranav <krisna.pranav@gmail.com>
2021-12-28 12:53:27 +0530

    api: chart
    charts

--- api/chart.go
+++ api/chart.go
@@ -1,5 +1,14 @@
 package api
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/piquette/finance-go/chart"
+	"github.com/piquette/finance-go/datetime"
+	"github.com/shopspring/decimal"
+)
+
 type Chart struct {
 	Ticker    string
 	Exchange  string
@@ -23,4 +32,58 @@ type Bar struct {
 	Current   decimal.Decimal
 	Y         int
 	Char      string
+}
+
+func GetChart(symbol string, interval datetime.Interval, start *datetime.Datetime, end *datetime.Datetime, extra bool) (*Chart, error) {
+	q := chart.Get(&chart.Params{Symbol: symbol, Interval: interval, Start: start, End: end, IncludeExt: extra})
+	if q.Count() < 7 && interval == datetime.FifteenMins {
+		q = chart.Get(&chart.Params{Symbol: symbol, Interval: datetime.FiveMins, Start: start, End: end, IncludeExt: false})
+		if q.Count() < 2 {
+			rn := time.Now()
+			e := rn.AddDate(0, 0, -28)
+			start = datetime.New(&e)
+			end = datetime.New(&rn)
+			q = chart.Get(&chart.Params{Symbol: symbol, Interval: datetime.OneDay, Start: start, End: end, IncludeExt: false})
+		}
+	}
+	var chart *Chart
+	for q.Next() {
+		if end != nil && q.Bar().Timestamp > end.Unix() {
+			break
+		}
+		if chart == nil {
+			chart = &Chart{
+				Interval: interval,
+				Start:    datetime.FromUnix(q.Bar().Timestamp),
+				High:     q.Bar().High,
+				Low:      q.Bar().Low,
+				Open:     q.Bar().Open,
+				Length:   q.Count(),
+				Ticker:   symbol,
+				Exchange: q.Meta().ExchangeName,
+				Currency: q.Meta().Currency,
+			}
+		}
+		bar := &Bar{Timestamp: datetime.FromUnix(q.Bar().Timestamp), Current: q.Bar().Close}
+		if bar.Current.IsZero() {
+			continue
+		}
+		if q.Bar().High.GreaterThan(chart.High) {
+			chart.High = q.Bar().High
+		}
+		if q.Bar().Low.LessThan(chart.Low) {
+			chart.Low = q.Bar().Low
+		}
+		chart.Close = q.Bar().Close
+		chart.End = datetime.FromUnix(q.Bar().Timestamp)
+		chart.Bars = append(chart.Bars, bar)
+	}
+	if chart == nil || len(chart.Bars) == 0 {
+		return nil, fmt.Errorf("No bars were found for this time period for %s", symbol)
+	}
+	orig := decimal.NewFromFloat(q.Meta().ChartPreviousClose)
+	chart.ChangeVal = chart.Close.Sub(orig)
+	chart.Change = chart.ChangeVal.Div(orig).Mul(decimal.NewFromInt(100))
+	chart.Prev = orig
+	return chart, nil
 }
