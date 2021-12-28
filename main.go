f4fc718094a6f38d9d53a8b9b07a33ea3e0167a2
krishpranav <krisna.pranav@gmail.com>
2021-12-28 13:18:30 +0530

    main go file: version call

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/krishpranav/gostocks/version"
 )
 
 func main() {
+	version := true
+
+	if version == true {
+		fmt.Println(version.Version)		
+		return
+	}
+	
 }
